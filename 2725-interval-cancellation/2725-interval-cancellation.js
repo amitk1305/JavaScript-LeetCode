3a81dad1fd56e93eff4c2f1c3af62f3ffb329a31
Amit Kumar <79916998+kumaramit13@users.noreply.github.com>
2023-10-23 20:07:48 +0530

    Time: 71 ms (14.88%), Space: 42 MB (61.86%) - LeetHub

---
+++ 2725-interval-cancellation/2725-interval-cancellation.js
@@ -0,0 +1,13 @@
+var cancellable = function(fn, args, t) {
+   fn(...args)
+
+   let id = setInterval(()=>{
+      fn(...args)
+   },t)
+
+   let cancelFn = ()=> clearInterval(id)
+
+   return cancelFn
+
+
+};
