2297d032c48007804f1053b35a5f6d4b400e137b
Amit Kumar <79916998+kumaramit13@users.noreply.github.com>
2023-10-27 16:33:34 +0530

    Time: 149 ms (29.43%), Space: 99 MB (40.18%) - LeetHub

---
+++ 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
@@ -0,0 +1,20 @@
+var flat = function (arr, n) {
+    
+    if(n==0){
+        return arr.slice();
+    }
+    
+    let newArr = [];
+
+    arr.forEach((val) => {
+        if(Array.isArray(val)){
+            const nested = flat(val, n-1);
+            newArr.push(...nested);
+        }
+        else{
+            newArr.push(val);
+        }
+    });
+
+    return newArr;
+};
